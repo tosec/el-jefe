c169ac996495722213d6e1b4e8da28060696296f
tosec <stephenvallejo44@gmail.com>
2023-08-05 01:21:51 -0400

    product details rendered on product card

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,14 +7,29 @@ export default function Product({ product, index }) {
       href={`/products/${product.id}`}
       className="border-2 rounded-sm group overflow-hidden"
     >
-      <div className="relative w-full h-96">
+      <div className="relative w-full h-72">
         <Image
           priority={index === 0}
           src={product.image}
           alt={product.name}
           fill
           sizes="100%"
+          style={{
+            objectFit: "contain",
+          }}
         ></Image>
+      </div>
+      <div className="p-6 bg-zinc-200">
+        <p className="font-semibold text-lg">{product.name}</p>
+        <div className="mt-4 flex items-center justify-between space-x-2">
+          <div>
+            <p className="text-gray-500">Price</p>
+            <p className="text-lg font-semibold">{product.price}</p>
+          </div>
+          <button className="border border-zinc-700 rounded-lg py-1 px-1">
+            Add to cart
+          </button>
+        </div>
       </div>
     </Link>
   );
