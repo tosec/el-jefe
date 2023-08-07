3df0349292561f061cfab9be69a9ba79ed322bf2
tosec <stephenvallejo44@gmail.com>
2023-08-07 15:41:13 -0400

    configured stripe checkout session,, axios

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,10 +1,46 @@
 import Link from "next/link";
 import { useShoppingCart } from "use-shopping-cart";
 import CartProduct from "src/components/CartProduct";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Cart() {
   const { cartCount, clearCart, formattedTotalPrice, cartDetails } =
     useShoppingCart();
+
+  const [isRedirecting, setRedirecting] = useState(false);
+
+  async function onCheckout() {
+    if (cartCount > 0) {
+      try {
+        setRedirecting(true);
+
+        const response = await axios.post(
+          "/api/checkout-sessions",
+          cartDetails,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "X-Requested-With": "XMLHttpRequest",
+            },
+          }
+        );
+
+        const session = response.data;
+
+        // ensure that the session url is a valid url before redirecting
+        if (session && session.url && new URL(session.url)) {
+          window.location.href = session.url;
+        } else {
+          console.log("Invalid session data received.");
+        }
+      } catch (error) {
+        console.log("Error: ", error);
+      } finally {
+        setRedirecting(false);
+      }
+    }
+  }
 
   return (
     <div className="container x;:max-w-screen-xl mx-auto py-16 px-8">
@@ -29,7 +65,7 @@ export default function Cart() {
           </h2>
           <p className="text-center mt-1 text-xl">
             Browse our collection{" "}
-            <Link href="/" className=" text-blue-800">
+            <Link href="/" className=" text-orange-400">
               here!
             </Link>
           </p>
@@ -46,8 +82,12 @@ export default function Cart() {
               Total:{" "}
               <span className="font-semibold">{formattedTotalPrice}</span>
             </p>
-            <button className="border rounded-md py-2 px-6 bg-zinc-400 hover:bg-zinc-500 text-white mt-4 max-w-max">
-              Checkout
+            <button
+              disabled={isRedirecting}
+              onClick={onCheckout}
+              className="border rounded-md py-2 px-6 bg-zinc-400 hover:bg-zinc-500 text-white mt-4 max-w-max"
+            >
+              {isRedirecting ? "Redirecting..." : "Checkout"}
             </button>
           </div>
         </div>
