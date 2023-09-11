cad17c434d58ab9e028290099f2c17394ebc83f6
tosec <tosecdev@gmail.com>
2023-09-10 23:15:50 -0400

    fixed unnecessary margin on shop page

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -7,13 +7,13 @@ export default function Shop({ products }) {
       <div class="max-w-2xl mx-auto md:px-6">
         <div class="md:py-20 py-18">
           <div class="md:pb-20 pb-12 max-w-3xl mx-auto text-center ">
-            <h2 class="md:text-5xl text-4xl text-black font-bold mb-4">
+            <h2 class="md:text-5xl text-4xl text-black font-bold">
               Micheladas
             </h2>
           </div>
         </div>
       </div>
-      <div className="container xl:max-w-screen-xl mx-auto py-12 px-3 mb-6">
+      <div className="container xl:max-w-screen-xl mx-auto px-3 mb-20">
         <div className="grid gap-10 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
           {products.map((product, index) => (
             <Product key={product.id} product={product} index={index} />
